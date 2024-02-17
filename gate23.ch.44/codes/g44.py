2553cdd3861dc89deea302e3c3d188c71db013c1
BATCHUISHITHA <155352743+BATCHUISHITHA@users.noreply.github.com>
2024-02-17 19:03:40 +0530

    Add files via upload

--- gate23.ch.44/codes/g44.py
+++ gate23.ch.44/codes/g44.py
@@ -5,12 +5,12 @@ from matplotlib.ticker import MultipleLocator
 # Load data from the "output.dat" file using numpy's loadtxt
 data = np.loadtxt("output.dat")
 
-# Extract n_values and y_values from the data
-t_values = data[:, 0].astype(int)
+# Extract t_values and Gt_values from the data
+t_values = data[:, 0]
 Gt_values = data[:, 1]
 
-# Create a stem plot
-plt.stem(t_values, Gt_values, linefmt='|', markerfmt='o', basefmt='b', label='Stem Plot')
+# Create a continuous plot
+plt.plot(t_values, Gt_values, marker='o', linestyle='-', label='Continuous Plot')
 
 plt.xlabel('$t$')
 plt.ylabel('$G(t)$')
@@ -20,3 +20,4 @@ plt.gca().xaxis.set_major_locator(MultipleLocator(1))
 plt.legend()
 plt.savefig('../figs/g44fig1.png')
 plt.show()
+
