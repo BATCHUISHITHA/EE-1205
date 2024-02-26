3fc14fc47d7039efa477814b766be2e1b9490183
BATCHUISHITHA <155352743+BATCHUISHITHA@users.noreply.github.com>
2024-02-26 18:05:26 +0530

    Add files via upload

--- gate22.nm.50/codes/g50.py
+++ gate22.nm.50/codes/g50.py
@@ -12,6 +12,12 @@ y_values = data[:, 1]
 # Create a continuous plot
 plt.plot(x_values, y_values, marker='', linestyle='-', label='Continuous Plot')
 
+# Find the index where x = 1
+index_x_1 = np.where(x_values == 1)[0][0]
+
+# Plot a red dot at y(1)
+plt.plot(x_values[index_x_1], y_values[index_x_1], 'ro', label='$y(1)$')
+
 plt.xlabel('$x$')
 plt.ylabel('$y(x)$')
 plt.grid(True)
