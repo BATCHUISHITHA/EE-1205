c0da9a00db269653c599e3dfcf035552ab0a8454
BATCHUISHITHA <155352743+BATCHUISHITHA@users.noreply.github.com>
2024-04-03 21:07:25 +0530

    Add files via upload

--- audio_filtering/codes/6.1.py
+++ audio_filtering/codes/6.1.py
@@ -1,45 +1,44 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import soundfile as sf
 from scipy import signal
+import numpy as np
+from matplotlib import pyplot as plt
 
-# Read the audio file
-input_signal, fs = sf.read('ishitha.wav')
-
-# Define the filter parameters
-order = 4
-cutoff_freq = 10000.0
-Wn = 2 * cutoff_freq / fs
-
-# Design the Butterworth filter
-b, a = signal.butter(order, Wn, 'low')
-
-# Custom filtering function using the difference equation
-def custom_filter(b, a, input_signal):
-    output_signal = signal.lfilter(b, a, input_signal)
+def myfiltfilt(b, a, input_signal):
+    # Perform zero-phase filtering using FFT
+    output_signal = signal.filtfilt(b, a, input_signal, padlen=1)
     return output_signal
 
-# Apply the custom filter to the input signal
-output_signal_custom = custom_filter(b, a, input_signal)
+# Read the input .wav file
+input_signal, fs = sf.read('ishitha.wav')
 
-# Apply the inbuilt filter to the input signal
-output_signal_builtin = signal.lfilter(b, a, input_signal)
+# Sampling frequency of input signal
+sampl_freq = fs
 
-# Time axis for plotting
-time = np.arange(len(input_signal)) / fs
+# Order of the filter
+order = 4
 
-# Plotting the verification plot with dots
-plt.figure(figsize=(10, 6))
+# Cutoff frequency 
+cutoff_freq = 10000.0
 
-plt.plot(time, output_signal_custom, 'bo', label='output of audio signal without using built in function')
-plt.plot(time, output_signal_builtin, 'ro', label='output of audio signal using built in function')
-plt.title('Verification of outputs of Audio Filter')
+# Digital frequency
+Wn = 2 * cutoff_freq / sampl_freq
+
+# Design Butterworth filter
+b, a = signal.butter(order, Wn, 'low')
+
+# Filter the input signal with Butterworth filter using filtfilt
+output_signal = myfiltfilt(b, a, input_signal)
+
+
+op1 = myfiltfilt(b, a, input_signal)
+
+# Verify outputs by plotting
+x_plt = np.arange(len(input_signal))
+plt.plot(x_plt[1000:10000], output_signal[1000:10000], 'b.',label='Output by built in function')
+plt.plot(x_plt[1000:10000], op1[1000:10000], 'r.',label='Output by not using built in function')
+plt.title("Verification of outputs of Audio Filter")
+plt.grid()
 plt.legend()
-
-plt.tight_layout()
-
-# Save the plot as an image
-plt.savefig('6.1.png')
-
+plt.savefig("6.1.png")
 plt.show()
 
